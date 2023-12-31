721deac0e4586ffc4a0a48a82f50dd8b762338c1
kinfey <kinfeylo@microsoft.com>
2023-12-31 12:47:11 +0800

    main

---
+++ notebooks/dotNET/Utils/Settings.cs
@@ -0,0 +1,6 @@
+public class Settings
+{
+    public static string AOAIEndpoint = "Your Azure OpenAI Endpoint";
+    public static string AOAIKey = "Your Azure OpenAI API Key";
+    public static string AOAIModel = "Your Azure OpenAI Model Deployment Name";
+}
