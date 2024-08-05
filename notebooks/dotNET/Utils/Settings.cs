9621ecc0de66767318fa720a87dc12ca2d71d66c
Kinfey <93169410+kinfey@users.noreply.github.com>
2024-08-05 14:17:02 +0000

    Update .NET and Python Samples

--- notebooks/dotNET/Utils/Settings.cs
+++
@@ -1,6 +0,0 @@
-public class Settings
-{
-    public static string AOAIEndpoint = "Your Azure OpenAI Service Endpoint";
-    public static string AOAIKey = "Your Azure OpenAI Service API Key";
-    public static string AOAIModel = "Your Azure OpenAI Service gpt-35-turbo Deployment Name";
-}
